b8aeca292568c108197bc5f44ddfacc8c9bced3e
Leandro Perego <leperego@hotmail.com>
2021-01-30 20:16:37 -0300

    Classe produto criada e classe principal

---
+++ src/aplicacao/Principal.java
@@ -0,0 +1,16 @@
+package aplicacao;
+
+import java.io.File;
+import java.util.Locale;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		String caminho = "C:\\Users\\Leandro\\Documents\\itens.csv";
+	
+		
+	}
+}
---
+++ src/entidades/Produto.java
@@ -0,0 +1,45 @@
+package entidades;
+
+public class Produto {
+
+	private String nome;
+	private Double valor;
+	private Integer quantidade;
+	
+	public Produto() {
+	}
+	
+	public Produto(String nome, double valor, int quantidade) {
+		this.nome = nome;
+		this.valor = valor;
+		this.quantidade = quantidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	public Double valorTotal() {
+		return this.valor * this.quantidade;
+	}
+}
